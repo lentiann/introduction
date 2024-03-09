c62fdb6974c57b80242d8db50000c9b9a1fb572b
Lentian <lentian.latifi@nash-renewables.com>
2024-03-09 01:31:04 +0100

    Test commit

---
+++ virtualenv_1/intro.py
@@ -0,0 +1,13 @@
+
+numri1 = 5
+numri2 = 5
+
+shuma = numri1 + numri2
+zbritja = numri1 - numri2
+shumezimi = numri1 * numri2
+pjestimi = numri1 // numri2
+
+print(f"Shuma e numrave {numri1} dhe {numri2} eshte = {shuma}")
+print(f"zbritja e numrave {numri1} dhe {numri2} eshte = {zbritja}")
+print(f"shumezimi e numrave {numri1} dhe {numri2} eshte = {shumezimi}")
+print(f"pjestimi e numrave {numri1} dhe {numri2} eshte = {pjestimi}")
